c5931cf955122728c86c037cae30bad682849320
Aaron <roycet000@gmail.com>
2020-05-29 03:15:32 -0700

    Add files via upload

---
+++ Pow2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pow2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+
+        for ( i = 2; i < 66000; i = i * 2 )
+            System.out.println(i + " * 2 =" + " " + (i * 2));
+
+
+
+
+
+
+
+    }
+
+}
